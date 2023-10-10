7993e4a50591558487af30664c58a036b376a0bd
Drew Walton <drewwalton19216801@gmail.com>
2023-10-09 18:42:30 -0600

    Don't use the blinker hardware in the decimal test

--- demos/decimal-test.asm
+++ demos/decimal-test.asm
@@ -1,44 +1,28 @@
   .org $C000
 
 reset:
-  lda #$ff
-  sta $8002
-
-  lda #$01
+  lda #$00
   sta temp
-  jmp add_to_99
+  jmp add_to_20
 
-add_to_99:
+add_to_20:
   ; Set decimal mode
   sed
   adc #$01
   cld
   sta temp
-  jsr blink
-  ; Jump to subtract_to_0 if we get to 50
-  cmp #$50
+  cmp #$20 ; Jump to subtract_to_0 if we get to 20
   beq subtract_to_0
-  jmp add_to_99
-
-blink:
-  ldx temp
-  stx $8000
-  rts
+  jmp add_to_20
 
 subtract_to_0:
   sed
   sbc #$01
   cld
   sta temp
-  jsr blink ; Blink the LED
-  cmp #$00 ; Jump to add_to_99 when we get to 0
-  beq add_to_99 
+  cmp #$00 ; Jump to add_to_20 when we get to 0
+  beq add_to_20
   jmp subtract_to_0
-
-blinkall:
-  ldx #$ff
-  stx $8000
-  jmp blinkall
 
 temp: .byte 0
 
